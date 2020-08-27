b75e32d9b4d52a0c6446bedba9a952afab8528a9
bxxstian <seb_faull@hotmail.co.uk>
2020-08-27 09:50:55 +0100

    Add selenium config.

--- src/test/java/com/sebfaull/seleniumFramework/Site/SeleniumConfig/SeleniumConfig.java
+++ src/test/java/com/sebfaull/seleniumFramework/Site/SeleniumConfig/SeleniumConfig.java
@@ -1,6 +1,9 @@
 package com.sebfaull.seleniumFramework.Site.SeleniumConfig;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
 
 public class SeleniumConfig {
 
@@ -9,8 +12,19 @@ public class SeleniumConfig {
 
     public SeleniumConfig(String browserName) {
         if (browserName.equalsIgnoreCase("chrome")) {
-            System.setProperty("webdriver.chrome.driver",
-                    seleniumProperties.getChromeDriverPath);
+            System.setProperty("webdriver.chrome.driver", seleniumProperties.getChromeDriverPath);
+            this.driver = new ChromeDriver();
         }
+
+        this.driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
     }
+
+    public WebDriver getDriver() {
+        return driver;
+    }
+
+    public void quitDriver() {
+        this.driver.quit();
+    }
+
 }
--- src/test/java/com/sebfaull/seleniumFramework/Site/SeleniumConfig/SeleniumPropertiesReader.java
+++ src/test/java/com/sebfaull/seleniumFramework/Site/SeleniumConfig/SeleniumPropertiesReader.java
@@ -1,4 +1,31 @@
 package com.sebfaull.seleniumFramework.Site.SeleniumConfig;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
 public class SeleniumPropertiesReader {
+
+    private String chromeDriverPath;
+    private String firefoxDriverPath;
+    private String propertiesFileLocation = "resources/selenium.properties";
+
+    public SeleniumPropertiesReader() {
+
+        Properties seleniumProperties = new Properties();
+
+        try {
+            seleniumProperties.load(new FileReader(propertiesFileLocation));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        chromeDriverPath = seleniumProperties.getProperty("chromedriverpath");
+
+    }
+
+    public String getChromeDriverPath() {
+        return chromeDriverPath;
+    }
+
 }
